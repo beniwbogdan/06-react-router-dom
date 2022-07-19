93e5d41212861306cbf06ce14782d85d9339229e
beniwbogdan <bohdanbeniv@gmail.com>
2022-07-19 15:02:05 +0200

    nesting

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <NavLink to={"/"}>main</NavLink>
-      <NavLink to={"/login"}>login</NavLink>
-      <NavLink to={"/profile"}>profile</NavLink>
+    return (
+        <div className="App">
+            <NavLink to={"/"}>MAIN</NavLink>---
+            <NavLink to={"/login"}>LOGIN</NavLink>---
+            <NavLink to={"/profile"}>PROFILE</NavLink>---
+            <NavLink to={"/profile/settings"}>PROFILE SETTINGS</NavLink>
 
-      <Routes>
-        <Route path={'/'} element={<div>main </div>}/>
-        <Route path={'/login'} element={<div>login</div>}/>
-        <Route path={'/profile'} element={<div>profile</div>}/>
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path={'/*'} element={<div>ERROR 404 SERVER NOT RESPOND </div>}/>
+                <Route path={'/'} element={<div>main </div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/profile/*'} element={
+                    <div>
+                        profile
+                        <Routes>
+                            <Route path={'/settings'} element={<div>settings</div>}/>
+                        </Routes>
+
+                    </div>
+                }/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
